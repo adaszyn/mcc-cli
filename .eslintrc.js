b5f7ec63bc02ccd7fae1a9ce974a02982a5caf9f
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2016-10-14 18:44:02 +0200

    Eslint config update, style fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
     "extends": "airbnb",
     "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+    ],
+    "rules": {
+        "indent": ["error", 4]
+    }
+
 };
